29696656ec8073e8b881dbef89f1e7974580548c
tobbicool <147714540+tobbicool@users.noreply.github.com>
2024-08-06 17:00:20 +0200

    v1.16.1

--- generate-secret.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto';
-
-function generateSecureString(length = 32) {
-  return crypto.randomBytes(length).toString('hex');
-}
-
-const secureString = generateSecureString();
-console.log('Your secure string:', secureString);
