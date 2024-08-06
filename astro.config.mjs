20940c682e8cc15c2152e13fc890e762fd7e233c
tobbicool <147714540+tobbicool@users.noreply.github.com>
2024-08-06 23:00:53 +0200

    Last commit made the translations being found, but with css not working. Here is my own fix to try and resolve css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,6 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name.endsWith('.json')) {
-              return 'locales/[name][extname]';
-            }
-            return 'assets/[name]-[hash][extname]';
-          },
-        },
-      },
-    },
-  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'no'],
