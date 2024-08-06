e24858ead880e304e0e1c98e39736ca5a0d63e5b
tobbicool <147714540+tobbicool@users.noreply.github.com>
2024-08-06 18:47:36 +0200

    Avoiding the 'lib'-folder functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,29 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
+  hooks: {
+    'astro:config:setup': ({ updateConfig }) => {
+      updateConfig({
+        vite: {
+          plugins: [
+            {
+              name: 'bypass-netlify-nft',
+              resolveId(source) {
+                if (source.includes('@astrojs/netlify/dist/lib/nft')) {
+                  return 'virtual:empty-module';
+                }
+              },
+              load(id) {
+                if (id === 'virtual:empty-module') {
+                  return 'export function copyDependenciesToFunction() { return Promise.resolve({ handler: "entry.mjs" }); }';
+                }
+              },
+            },
+          ],
+        },
+      });
+    },
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'no'],
