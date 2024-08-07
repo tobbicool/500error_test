4f45840cf3aff40472981fb679c056c7cade1fc5
tobbicool <147714540+tobbicool@users.noreply.github.com>
2024-08-07 20:50:06 +0200

    v5.3.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { logFolderContents } from './folder-tracker.mjs';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   output: 'server',
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url),
-    functionPerRoute: false,
-    binaryMediaTypes: ['image/*', 'video/*', 'application/*', 'font/*'],
-    functionRuntimePath: "dist/.netlify/functions/",
-  }),
-  vite: {
-    ssr: {
-      external: ['@astrojs/netlify']
-    }
-  },
+  adapter: netlify(),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'no'],
